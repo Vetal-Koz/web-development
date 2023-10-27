9e79e8fd2448574373d927bc313e720d9ed2c207
Vetal-Koz <kozvitalik567@gmail.com>
2023-10-28 00:45:42 +0300

    Security Util package

--- src/main/java/web/dev/webdev/repository/UserRepository.java
+++ src/main/java/web/dev/webdev/repository/UserRepository.java
@@ -4,5 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import web.dev.webdev.models.UserEntity;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    UserEntity findFirstByUsername(String username);
     UserEntity findByEmail(String email);
 }
